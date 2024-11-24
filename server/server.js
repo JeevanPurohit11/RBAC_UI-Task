4d8bb40b474d3436762de4946842c20a7a5b1a60
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-11-24 18:02:37 +0530

    Backend server code done

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+const roleRoutes = require('./routes/roleRoutes');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+app.use('/api/users', userRoutes);
+app.use('/api/roles', roleRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
